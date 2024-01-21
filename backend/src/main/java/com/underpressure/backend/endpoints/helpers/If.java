0a61efecf4257bad010d1d64349048bdba3744e1
var-pi <stefanehin4@gmail.com>
2024-01-21 19:52:10 +0200

    Use queryForObject to check if exists

--- backend/src/main/java/com/underpressure/backend/endpoints/helpers/If.java
+++ backend/src/main/java/com/underpressure/backend/endpoints/helpers/If.java
@@ -1,33 +1,31 @@
 package com.underpressure.backend.endpoints.helpers;
 
-import java.util.List;
-
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class If {
     public static boolean subjecetInstanceExists(String userId, Integer subjectId,
             JdbcTemplate jdbcTemplate) {
-        String sql = "SELECT id from subject_instances WHERE user_id=? AND subject_id=?";
+        String sql = "SELECT COUNT(*) from subject_instances WHERE user_id=? AND subject_id=?";
 
-        List<String> subjectInstances = jdbcTemplate.queryForList(sql, String.class, userId, subjectId);
+        Integer rowCount = jdbcTemplate.queryForObject(sql, Integer.class, userId, subjectId);
 
-        return subjectInstances.size() != 0;
+        return rowCount > 0;
     }
 
     public static boolean subjectInstanceFollowed(Integer subjectInstanceId, JdbcTemplate jdbcTemplate) {
-        String sql = "SELECT id FROM subject_instances WHERE id=? AND if_followed=TRUE";
+        String sql = "SELECT COUNT(*) FROM subject_instances WHERE id=? AND if_followed=TRUE";
 
-        List<Integer> ids = jdbcTemplate.queryForList(sql, Integer.class, subjectInstanceId);
+        Integer rowCount = jdbcTemplate.queryForObject(sql, Integer.class, subjectInstanceId);
 
-        return ids.size() != 0;
+        return rowCount > 0;
     }
 
     public static boolean entryExists(Integer subjectInstanceId, JdbcTemplate jdbcTemplate) {
-        String sql = "SELECT id FROM entries WHERE subject_instance_id=? AND created_at=CURRENT_DATE";
+        String sql = "SELECT COUNT(*) FROM entries WHERE subject_instance_id=? AND created_at=CURRENT_DATE";
 
-        List<Integer> ids = jdbcTemplate.queryForList(sql, Integer.class, subjectInstanceId);
+        Integer rowCount = jdbcTemplate.queryForObject(sql, Integer.class, subjectInstanceId);
 
-        return ids.size() != 0;
+        return rowCount > 0;
     }
 
     public static boolean userExists(String userId, JdbcTemplate jdbcTemplate) {
