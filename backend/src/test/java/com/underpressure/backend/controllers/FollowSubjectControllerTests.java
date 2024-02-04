7e29206e926b03a11b37084d1dc4853681ed5308
var-pi <stefanehin4@gmail.com>
2024-02-04 12:01:00 +0200

    Cleaned tests file

--- backend/src/test/java/com/underpressure/backend/controllers/FollowSubjectControllerTests.java
+++ backend/src/test/java/com/underpressure/backend/controllers/FollowSubjectControllerTests.java
@@ -1,43 +1,25 @@
 package com.underpressure.backend.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
-import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
 import com.underpressure.backend.controllers.classes.ApiResponse;
 import com.underpressure.backend.controllers.classes.request.body.FollowSubjectRequestBody;
 import com.underpressure.backend.controllers.helpers.Fetch;
-import com.underpressure.backend.controllers.helpers.FetchStatic;
-import com.underpressure.backend.controllers.helpers.Validate;
 import com.underpressure.backend.exceptions.unexpected.UserVerificationException;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.json.webtoken.JsonWebSignature.Header;
-import com.google.api.client.json.webtoken.JsonWebToken.Payload;
-import static org.mockito.Mockito.*;
 
 @JdbcTest
 @AutoConfigureTestDatabase
@@ -61,8 +43,8 @@ public class FollowSubjectControllerTests {
 
         @BeforeEach
         public void setUp() throws UserVerificationException {
-                doReturn("10001").when(fetchGoogleMock).sub(eq("user_1_id_token"), any());
-                doReturn("10002").when(fetchGoogleMock).sub(eq("user_2_id_token"), any());
+                doReturn("10001").when(fetchGoogleMock).sub(eq("user_1_id_token"), anyString());
+                doReturn("10002").when(fetchGoogleMock).sub(eq("user_2_id_token"), anyString());
         }
 
         @Test
