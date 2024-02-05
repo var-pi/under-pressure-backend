68993b11108139a4f96040dde037d39a60d0e79e
var-pi <stefanehin4@gmail.com>
2024-02-05 18:43:28 +0200

    Cleaned imprts

--- backend/src/main/java/com/underpressure/backend/controllers/services/database/ValidateImpl.java
+++ backend/src/main/java/com/underpressure/backend/controllers/services/database/ValidateImpl.java
@@ -1,6 +1,5 @@
 package com.underpressure.backend.controllers.services.database;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
