fe30966bc7c212740f6d57b779638de63b2e7a4b
var-pi <stefanehin4@gmail.com>
2024-02-05 14:01:23 +0200

    Renamed GetComponentUpdated to GetComponent

--- backend/src/main/java/com/underpressure/backend/controllers/FetchSubjectsController.java
+++ backend/src/main/java/com/underpressure/backend/controllers/FetchSubjectsController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.underpressure.backend.controllers.classes.abstracts.GetControllerUpdated;
+import com.underpressure.backend.controllers.classes.abstracts.GetController;
 import com.underpressure.backend.controllers.classes.request.params.GetSubjectsParams;
 import com.underpressure.backend.controllers.helpers.Fetch;
 
 @RestController
-public class FetchSubjectsController extends GetControllerUpdated<List<String>, GetSubjectsParams> {
+public class FetchSubjectsController extends GetController<List<String>, GetSubjectsParams> {
 
     @Autowired
     Fetch.DB fetchDB;
--- backend/src/main/java/com/underpressure/backend/controllers/classes/abstracts/GetController.java
+++ backend/src/main/java/com/underpressure/backend/controllers/classes/abstracts/GetController.java
@@ -4,11 +4,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-import com.underpressure.backend.controllers.classes.ApiResponse;
-
 public abstract class GetController<S, T> extends Controller {
 
     @CrossOrigin(origins = "*") // TODO Change to an appropriate url
-    public abstract ResponseEntity<ApiResponse<S>> handle(@ModelAttribute T params);
+    public abstract ResponseEntity<S> handle(@ModelAttribute T params);
 
 }
