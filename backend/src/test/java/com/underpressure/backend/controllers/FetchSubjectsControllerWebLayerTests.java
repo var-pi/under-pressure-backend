c7e21f8561f7e208a8f42779034fc3297636308a
var-pi <stefanehin4@gmail.com>
2024-02-06 11:50:29 +0200

    Cleaned imports

--- backend/src/test/java/com/underpressure/backend/controllers/FetchSubjectsControllerWebLayerTests.java
+++ backend/src/test/java/com/underpressure/backend/controllers/FetchSubjectsControllerWebLayerTests.java
@@ -9,17 +9,13 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.underpressure.backend.services.database.DatabaseService;
