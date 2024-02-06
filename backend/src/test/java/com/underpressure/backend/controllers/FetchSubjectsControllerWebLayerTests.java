374383e707472bf93b9c1fa375edb28491f4cfb9
var-pi <stefanehin4@gmail.com>
2024-02-06 11:50:10 +0200

    Web layer test for FetchSubjectsController

---
+++ backend/src/test/java/com/underpressure/backend/controllers/FetchSubjectsControllerWebLayerTests.java
@@ -0,0 +1,67 @@
+package com.underpressure.backend.controllers;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.underpressure.backend.services.database.DatabaseService;
+import com.underpressure.backend.services.database.FetchDatabase;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FetchSubjectsControllerWebLayerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private DatabaseService databaseServiceMock;
+
+    @InjectMocks
+    private FetchSubjectsController controller;
+
+    FetchDatabase fetchDatabaseMock;
+
+    List<String> expectedSubjects;
+
+    @BeforeEach
+    void setUp() {
+        fetchDatabaseMock = mock(FetchDatabase.class);
+        when(databaseServiceMock.fetch()).thenReturn(fetchDatabaseMock);
+
+        expectedSubjects = Arrays.asList("Subject 1", "Subject 2");
+        when(fetchDatabaseMock.subjects()).thenReturn(expectedSubjects);
+    }
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/subjects")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(new ObjectMapper().writeValueAsString(expectedSubjects)));
+
+    }
+
+}
