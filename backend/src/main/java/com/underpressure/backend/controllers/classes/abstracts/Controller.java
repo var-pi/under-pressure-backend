a05a7cb62fec460632dfde800ef5f15958a56e26
var-pi <stefanehin4@gmail.com>
2024-02-06 09:24:31 +0200

    Removed JdbcTemplate from Controller abstract class

--- backend/src/main/java/com/underpressure/backend/controllers/classes/abstracts/Controller.java
+++ backend/src/main/java/com/underpressure/backend/controllers/classes/abstracts/Controller.java
@@ -1,11 +1,4 @@
 package com.underpressure.backend.controllers.classes.abstracts;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-
 public abstract class Controller {
-
-    @Autowired
-    protected JdbcTemplate jdbcTemplate;
-
 }
