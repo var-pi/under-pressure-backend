3e9490cdefa5dc9ad6be07868ed9913ea9a75630
var-pi <stefanehin4@gmail.com>
2024-02-08 12:51:17 +0200

    Updated database location

--- backend/src/main/java/com/underpressure/backend/controllers/FetchSubjectsAllController.java
+++ backend/src/main/java/com/underpressure/backend/controllers/FetchSubjectsAllController.java
@@ -1,6 +1,5 @@
 package com.underpressure.backend.controllers;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
@@ -9,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.underpressure.backend.abstracts.GetController;
+import com.underpressure.backend.requests.data.FetchSubjectsAllRequestData;
 import com.underpressure.backend.requests.path_variables.FetchSubjectsAllPathVariables;
 import com.underpressure.backend.services.application.ApplicationService;
 
@@ -25,10 +25,7 @@ public class FetchSubjectsAllController extends GetController<List<String>, Fetc
     @GetMapping("/subjects/all")
     public ResponseEntity<List<String>> handle(FetchSubjectsAllPathVariables pathVariables) {
 
-        List<String> subjects = Arrays.asList("Math", "PE");
-
-        // List<String> subjects = applicationService.fetchSubjectsAll(new
-        // FetchSubjectsAllRequestData(pathVariables));
+        List<String> subjects = applicationService.fetchSubjectsAll(new FetchSubjectsAllRequestData(pathVariables));
 
         return new ResponseEntity<>(subjects, HttpStatus.OK);
 
