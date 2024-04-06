ca4a30b6a589c83082a7a5b5b888477344ce4b67
var-pi <stefanehin4@gmail.com>
2024-04-06 10:09:02 +0300

    Sort entries in SQL request with ORDER BY.

--- database.sql
+++ database.sql
@@ -1,26 +1,26 @@
-CREATE TABLE "users" ( 
-  "id" SERIAL PRIMARY KEY,
-  "google_sub" varchar(255),
-  "given_name" varchar(255)
+CREATE TABLE "users" (
+                         "id" SERIAL PRIMARY KEY,
+                         "google_sub" varchar(255),
+                         "given_name" varchar(255)
 );
 
 CREATE TABLE "subjects" (
-  "id" SERIAL PRIMARY KEY,
-  "name" varchar(255)
+                            "id" SERIAL PRIMARY KEY,
+                            "name" varchar(255)
 );
 
 CREATE TABLE "subject_instances" (
-  "id" SERIAL PRIMARY KEY,
-  "user_id" integer,
-  "subject_id" integer,
-  "if_followed" boolean
+                                     "id" SERIAL PRIMARY KEY,
+                                     "user_id" integer,
+                                     "subject_id" integer,
+                                     "if_followed" boolean
 );
 
 CREATE TABLE "entries" (
-  "id" SERIAL PRIMARY KEY,
-  "subject_instance_id" integer,
-  "creation_date" date,
-  "stress_level" integer
+                           "id" SERIAL PRIMARY KEY,
+                           "subject_instance_id" integer,
+                           "creation_date" date,
+                           "stress_level" integer
 );
 
 -- ALTER TABLE "subject_instances" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
