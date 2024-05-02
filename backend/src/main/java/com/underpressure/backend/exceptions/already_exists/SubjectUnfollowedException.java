91bbb205ec3bd135159b09e00af6519d7f89d721
HjalmarVaikuell <vaikuell.hjalmar@gmail.com>
2024-05-02 13:44:16 +0300

    Removed "juba"

--- backend/src/main/java/com/underpressure/backend/exceptions/already_exists/SubjectUnfollowedException.java
+++ backend/src/main/java/com/underpressure/backend/exceptions/already_exists/SubjectUnfollowedException.java
@@ -3,7 +3,7 @@ package com.underpressure.backend.exceptions.already_exists;
 public class SubjectUnfollowedException extends AlreadyException {
 
     public SubjectUnfollowedException() {
-        super("Seda ainet juba pole su ainete nimekirjas.");
+        super("Seda ainet pole su ainete nimekirjas.");
     }
 
 }
